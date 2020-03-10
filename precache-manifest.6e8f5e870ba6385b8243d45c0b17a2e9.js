d8731fce551139ae7438099ed6f57c5429ab3edd
mladjo97 <mldnmilosevic@gmail.com>
2020-03-10 13:53:28 +0100

    auto deploy script

---
+++ precache-manifest.6e8f5e870ba6385b8243d45c0b17a2e9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "545f46f16365e2c66a24703b73a5a0d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc41db4e050fbb0d9541",
+    "url": "/static/css/2.d795e60d.chunk.css"
+  },
+  {
+    "revision": "a0491b476297678d58fe",
+    "url": "/static/css/main.c619e7c6.chunk.css"
+  },
+  {
+    "revision": "fc41db4e050fbb0d9541",
+    "url": "/static/js/2.85b365eb.chunk.js"
+  },
+  {
+    "revision": "fc80bae56b5cdd8b0708bbd014549668",
+    "url": "/static/js/2.85b365eb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a0491b476297678d58fe",
+    "url": "/static/js/main.68d7326e.chunk.js"
+  },
+  {
+    "revision": "6e7d85ad969107dd34f7",
+    "url": "/static/js/runtime-main.54edee9e.js"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/static/media/UploadIcon.1cedb6e9.svg"
+  }
+]);
